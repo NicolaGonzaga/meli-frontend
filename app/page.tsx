c5697532be2879066ac1748faf5ab768f4a5a28a
Nicola Gonzaga <nicolagonzaga@gmail.com>
2024-06-24 22:10:18 -0400

    chore: updating path

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import Header from "../components/Header";
+import Header from "../components/Header/Header";
 import styles from "./page.module.css";
 import "./globals.css";
 
