894dad638c0ec08fe2258cdb8b55140e68782314
Nicola Gonzaga <nicolagonzaga@gmail.com>
2024-06-24 22:08:28 -0400

    feat: component list

---
+++ components/List/List.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Item } from "../../types";
+import styles from "./List.module.css";
+import ItemList from "../ItemList/ItemList";
+
+interface ListProps {
+  items: Item[];
+}
+
+const List: React.FC<ListProps> = ({ items }) => {
+  return (
+    <div className={styles.listContainer}>
+      <ul className={styles.listItem}>
+        {items.map((item, index) => (
+          <React.Fragment key={item.id}>
+            <ItemList item={item} />
+            {index < items.length - 1 && <hr className={styles.hr} />}
+          </React.Fragment>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default List;
