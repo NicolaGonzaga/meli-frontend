d1d6c4732df6bb27fddd9edd8a678b782e550605
Nicola Gonzaga <nicolagonzaga@gmail.com>
2024-06-27 09:52:55 -0400

    feat: added link router to logo

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -5,6 +5,7 @@ import Search from "../Search/Search";
 import logo from "../../public/logo.png";
 import Image from "next/image";
 import styles from "./Header.module.css";
+import Link from "next/link";
 
 interface HeaderProps {
   onSearch: (query: string) => void;
@@ -13,7 +14,9 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ onSearch }) => {
   return (
     <header data-testid="header" className={styles.headerContainer}>
-      <Image src={logo} alt="Logotipo Mercado Livre" width={50} height={32} />
+      <Link href="/">
+        <Image src={logo} alt="Logotipo Mercado Livre" width={50} height={32} />
+      </Link>
       <Search onSearch={onSearch} />
     </header>
   );
